9a923365423cd71a3a3affef2cc361104597fb2d
ohharim <xhhxrxm@naver.com>
2023-08-22 18:09:23 +0900

    hi

--- src/component/Button.js
+++ src/component/Button.js
@@ -1,8 +1,10 @@
 import '../css/Button.css';
+import React from 'react';
 
 function Button(props) {
   return (
     <button className='button'>
+      {props.icon && <props.icon />}
       {props.title}
     </button>
   );
--- src/page/Main.js
+++ src/page/Main.js
@@ -1,9 +1,16 @@
 import Header from '../component/Header';
 import Button from '../component/Button';
 import Mobile from '../component/Mobile';
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { MdOutlineLocalCafe, MdOutlineLocalGroceryStore} from "react-icons/md";
+import { GrCafeteria } from "react-icons/gr"; 
+import { BiCameraMovie, BiSearchAlt } from "react-icons/bi"; 
+import { Ri24HoursFill } from "react-icons/ri"; 
+import { TbBuildingCarousel,TbDotsDiagonal } from "react-icons/tb"; 
+import { BsBuildings } from "react-icons/bs"; 
+import { MdOutlineLocalGasStation } from "react-icons/md"; 
+import { FiBookOpen } from "react-icons/fi"; 
 
 function Main() {
     const [responseData, setResponseData] = useState(null);
@@ -25,16 +32,19 @@ function Main() {
             <div>
                 <Header></Header>
                 <input type='text' placeholder='장소를 검색하세요.'/>
-                <button>검색</button>
-                    <Button title='카페' />
-                    <Button title='음식점' />
-                    <Button title='영화관' />
-                    <Button title='테마파크' />
-                    <Button title='통신사' />
-                    <Button title='미용실' />
-                    <Button title='주유소' />
-                    <Button title='도서' />
-                    <Button title='기타' />
+                <button><BiSearchAlt size="3em"/></button>
+                <div className='button-container'>
+                    <Button title='카페' icon={() => <MdOutlineLocalCafe size="2em" />} />
+                    <Button title='음식점' icon={() => <GrCafeteria size="2em" />} />
+                    <Button title='편의점' icon={() => <Ri24HoursFill size="2em" />} />
+                    <Button title='영화관' icon={() => <BiCameraMovie size="2em" />} />
+                    <Button title='테마파크' icon={() => <TbBuildingCarousel size="2em" />} />
+                    <Button title='마트' icon={() => <MdOutlineLocalGroceryStore size="2em" />} />
+                    <Button title='백화점' icon={() => <BsBuildings size="2em" />} />
+                    <Button title='주유소' icon={() => <MdOutlineLocalGasStation size="2em" />} />
+                    <Button title='도서' icon={() => <FiBookOpen size="2em" />} />
+                    <Button title='기타' icon={() => <TbDotsDiagonal size="2em"/>} />
+                </div>
             </div>
         </Mobile>
     );
